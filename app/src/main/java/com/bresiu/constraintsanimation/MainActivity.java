272f454e477ed1413e3f65002d44af06901d20b9
Bresiu <michal.brewczak@gmail.com>
2017-02-07 21:26:48 +0100

    added butterknife library; added on click methods for ConstraintSet controll

--- app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
+++ app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
@@ -1,13 +1,41 @@
 package com.bresiu.constraintsanimation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main)
+    ConstraintLayout constraintLayout;
+    private ConstraintSet applyConstraintSet = new ConstraintSet();
+    private ConstraintSet resetConstraintSet = new ConstraintSet();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        resetConstraintSet.clone(constraintLayout);
+        applyConstraintSet.clone(constraintLayout);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.applyButton)
+    public void onApplyClick(View view) {
+        applyConstraintSet.setMargin(R.id.button1, ConstraintSet.START, 8);
+        applyConstraintSet.applyTo(constraintLayout);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.resetButton)
+    public void onResetClick(View view) {
+        resetConstraintSet.applyTo(constraintLayout);
     }
 }
