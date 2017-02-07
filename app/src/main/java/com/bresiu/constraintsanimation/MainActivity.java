6be70046643a9cd15b10a5e0533315c6d55af61d
Bresiu <michal.brewczak@gmail.com>
2017-02-07 22:14:05 +0100

    added chains; refactored

--- app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
+++ app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
@@ -3,6 +3,7 @@ package com.bresiu.constraintsanimation;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
+import android.support.constraint.solver.widgets.ConstraintWidget;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.TransitionManager;
 import android.view.View;
@@ -31,8 +32,13 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.applyButton)
     public void onApplyClick(View view) {
         TransitionManager.beginDelayedTransition(constraintLayout);
+        //firstSample();
+        //secondSample();
+        thirdSample();
+        applyConstraintSet.applyTo(constraintLayout);
+    }
 
-        /*
+    private void firstSample() {
         applyConstraintSet.setMargin(R.id.button1, ConstraintSet.START, 0);
         applyConstraintSet.setMargin(R.id.button1, ConstraintSet.END, 0);
         applyConstraintSet.setMargin(R.id.button2, ConstraintSet.START, 0);
@@ -53,8 +59,9 @@ public class MainActivity extends AppCompatActivity {
         applyConstraintSet.constrainHeight(R.id.button1, 200);
         applyConstraintSet.constrainHeight(R.id.button2, 200);
         applyConstraintSet.constrainHeight(R.id.button3, 200);
-        */
+    }
 
+    private void secondSample() {
         applyConstraintSet.setVisibility(R.id.button2, ConstraintSet.GONE);
         applyConstraintSet.setVisibility(R.id.button3, ConstraintSet.GONE);
         applyConstraintSet.clear(R.id.button1);
@@ -62,8 +69,34 @@ public class MainActivity extends AppCompatActivity {
         applyConstraintSet.connect(R.id.button1, ConstraintSet.RIGHT, R.id.main, ConstraintSet.RIGHT, 0);
         applyConstraintSet.connect(R.id.button1, ConstraintSet.TOP, R.id.main, ConstraintSet.TOP, 0);
         applyConstraintSet.connect(R.id.button1, ConstraintSet.BOTTOM, R.id.applyButton, ConstraintSet.TOP, 0);
+    }
 
-        applyConstraintSet.applyTo(constraintLayout);
+    private void thirdSample() {
+        applyConstraintSet.clear(R.id.button1);
+        applyConstraintSet.clear(R.id.button2);
+        applyConstraintSet.clear(R.id.button3);
+
+        applyConstraintSet.connect(R.id.button1, ConstraintSet.LEFT, R.id.main, ConstraintSet.LEFT, 0);
+        applyConstraintSet.connect(R.id.button3, ConstraintSet.RIGHT, R.id.main, ConstraintSet.RIGHT, 0);
+
+        applyConstraintSet.connect(R.id.button2, ConstraintSet.LEFT, R.id.button1, ConstraintSet.RIGHT, 0);
+        applyConstraintSet.connect(R.id.button1, ConstraintSet.RIGHT, R.id.button2, ConstraintSet.LEFT, 0);
+
+        applyConstraintSet.connect(R.id.button2, ConstraintSet.RIGHT, R.id.button3, ConstraintSet.LEFT, 0);
+        applyConstraintSet.connect(R.id.button3, ConstraintSet.LEFT, R.id.button2, ConstraintSet.RIGHT, 0);
+
+        applyConstraintSet.createHorizontalChain(R.id.button1, R.id.button3,
+                new int[]{R.id.button1, R.id.button3}, null, ConstraintWidget.CHAIN_PACKED);
+
+        applyConstraintSet.setHorizontalBias(R.id.button1, .1f);
+
+        applyConstraintSet.constrainWidth(R.id.button1, ConstraintSet.WRAP_CONTENT);
+        applyConstraintSet.constrainWidth(R.id.button2, ConstraintSet.WRAP_CONTENT);
+        applyConstraintSet.constrainWidth(R.id.button3, ConstraintSet.WRAP_CONTENT);
+
+        applyConstraintSet.constrainHeight(R.id.button1, 200);
+        applyConstraintSet.constrainHeight(R.id.button2, 200);
+        applyConstraintSet.constrainHeight(R.id.button3, 200);
     }
 
     @SuppressWarnings("unused")
