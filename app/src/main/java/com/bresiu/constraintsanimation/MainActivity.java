2fb461cf9e41d8f601ce8208ad3e41edeb5a8e76
Bresiu <michal.brewczak@gmail.com>
2017-02-07 21:51:59 +0100

    button 1 to take almost entire screen

--- app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
+++ app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public void onApplyClick(View view) {
         TransitionManager.beginDelayedTransition(constraintLayout);
 
+        /*
         applyConstraintSet.setMargin(R.id.button1, ConstraintSet.START, 0);
         applyConstraintSet.setMargin(R.id.button1, ConstraintSet.END, 0);
         applyConstraintSet.setMargin(R.id.button2, ConstraintSet.START, 0);
@@ -52,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
         applyConstraintSet.constrainHeight(R.id.button1, 200);
         applyConstraintSet.constrainHeight(R.id.button2, 200);
         applyConstraintSet.constrainHeight(R.id.button3, 200);
+        */
+
+        applyConstraintSet.setVisibility(R.id.button2, ConstraintSet.GONE);
+        applyConstraintSet.setVisibility(R.id.button3, ConstraintSet.GONE);
+        applyConstraintSet.clear(R.id.button1);
+        applyConstraintSet.connect(R.id.button1, ConstraintSet.LEFT, R.id.main, ConstraintSet.LEFT, 0);
+        applyConstraintSet.connect(R.id.button1, ConstraintSet.RIGHT, R.id.main, ConstraintSet.RIGHT, 0);
+        applyConstraintSet.connect(R.id.button1, ConstraintSet.TOP, R.id.main, ConstraintSet.TOP, 0);
+        applyConstraintSet.connect(R.id.button1, ConstraintSet.BOTTOM, R.id.applyButton, ConstraintSet.TOP, 0);
 
         applyConstraintSet.applyTo(constraintLayout);
     }
