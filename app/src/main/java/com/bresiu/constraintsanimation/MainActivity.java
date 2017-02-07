a85f2689a283184dea78fd2a7c62d6228d072b15
Bresiu <michal.brewczak@gmail.com>
2017-02-07 21:46:26 +0100

    button height and width resize

--- app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
+++ app/src/main/java/com/bresiu/constraintsanimation/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.applyButton)
     public void onApplyClick(View view) {
         TransitionManager.beginDelayedTransition(constraintLayout);
+
         applyConstraintSet.setMargin(R.id.button1, ConstraintSet.START, 0);
         applyConstraintSet.setMargin(R.id.button1, ConstraintSet.END, 0);
         applyConstraintSet.setMargin(R.id.button2, ConstraintSet.START, 0);
@@ -39,9 +40,19 @@ public class MainActivity extends AppCompatActivity {
         applyConstraintSet.setMargin(R.id.button3, ConstraintSet.START, 0);
         applyConstraintSet.setMargin(R.id.button3, ConstraintSet.END, 0);
         applyConstraintSet.setMargin(R.id.button3, ConstraintSet.TOP, 0);
+
         applyConstraintSet.centerHorizontally(R.id.button1, R.id.main);
         applyConstraintSet.centerHorizontally(R.id.button2, R.id.main);
         applyConstraintSet.centerHorizontally(R.id.button3, R.id.main);
+
+        applyConstraintSet.constrainWidth(R.id.button1, 600);
+        applyConstraintSet.constrainWidth(R.id.button2, 600);
+        applyConstraintSet.constrainWidth(R.id.button3, 600);
+
+        applyConstraintSet.constrainHeight(R.id.button1, 200);
+        applyConstraintSet.constrainHeight(R.id.button2, 200);
+        applyConstraintSet.constrainHeight(R.id.button3, 200);
+
         applyConstraintSet.applyTo(constraintLayout);
     }
 
